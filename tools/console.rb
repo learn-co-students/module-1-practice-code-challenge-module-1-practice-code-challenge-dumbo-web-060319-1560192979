19a859712af1231eb35ff0fd55cf062c1eceabd4
Justin Hutsell <hutselljustin@gmail.com>
2019-06-10 16:37:51 -0400

    Done

--- tools/console.rb
+++ tools/console.rb
@@ -7,4 +7,15 @@ end
 
 # Put your variables here~!
 
+joe = Passenger.new("Joe")
+steve = Passenger.new("Steven")
+
+lenny = Driver.new("Lenny")
+driver_neumero_dos = Driver.new("Driva")
+
+ride1 = Ride.new(lenny, joe, 23.4)
+ride2 = Ride.new(lenny, steve, 32.6)
+ride3 = Ride.new(lenny, joe, 73.6)
+ride4 = Ride.new(lenny, joe, 54.3)
+
 binding.pry
