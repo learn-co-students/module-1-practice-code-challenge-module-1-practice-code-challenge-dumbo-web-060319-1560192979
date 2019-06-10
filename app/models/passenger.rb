bac6320e074b87b56e12097b3efadf49b01b9c4f
Francis Mendez <fmendez884@gmail.com>
2019-06-10 17:03:03 -0400

    only 2 methods left to complete

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,43 @@
+class Passenger
+    attr_accessor :name, :driver, :rides
+    @@all = []
 
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def rides 
+        Ride.all.select do |ride|
+            ride.passenger == self
+        end 
+    end 
+
+    def drivers
+        Driver.all.select do |driver|
+            driver.rides.select do |ride|
+                ride.passenger == self
+            end
+        end
+    end 
+
+    def total_distance
+        total = 0
+        self.rides.each do |ride|
+            total += ride.distance 
+        end 
+        return total
+    end 
+
+    def self.premium_members
+        self.all.select do |passenger|
+            passenger.total_distance > 100
+        end
+    end 
+
+end 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,28 @@
+class Ride
+    attr_accessor :driver, :passenger, :distance
+    @@all = [] 
 
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger 
+        @distance = distance 
+
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.average_distance
+        total_distance = 0
+        average_count = 0
+        Ride.all.each do |ride|
+            total_distance += ride.distance
+            average_count +=1
+        end 
+        average = total_distance / average_count.to_f
+        return average
+    end 
+
+end 
