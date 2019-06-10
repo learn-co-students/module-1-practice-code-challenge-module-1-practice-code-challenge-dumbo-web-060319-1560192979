36a0639a999d6822509b861db77baf11990b3307
Alexander Schelchere <ascellie@gmail.com>
2019-06-10 16:14:46 -0400

    Ride class - skeleton (Skeleton, outline, etc.)

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,23 @@
+class Ride
 
+    attr_accessor :driver, :passenger, :distance # Ride deliverables 1-3
+
+    @@all = []
+
+    # A Ride should be initialized with a driver (as a Driver object), 
+    # a passenger (as a Passenger object), and a distance (as a float i.e. 3.2). 
+    # The distance refers to miles.
+
+    def initialize(driver, passenger, distance) 
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+
+        @@all << self
+    end
+
+    def self.average_distance
+        # Returns the average distance across ALL rides
+
+    end
+end
